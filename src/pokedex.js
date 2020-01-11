361ab5e8e223e2b242137beae905e7d6f5285f9f
TOSHIBA <TOSHIBA@DESKTOP-67JV907>
2020-01-11 14:57:47 +0530

    final commit

--- src/pokedex.js
+++ src/pokedex.js
@@ -1,5 +1,5 @@
 const pokedex = document.getElementById('pokedex');
-
+var data = [];
 const fetchPokemon = () => {
     const promises = [];
     for (let i = 1; i <= 150; i++) {
@@ -13,6 +13,7 @@ const fetchPokemon = () => {
             type: result.types.map((type) => type.type.name).join(', '),
             id: result.id
         }));
+        data=pokemon;
         displayPokemon(pokemon);
     });
 };
@@ -102,46 +103,20 @@ fetchPokemon();
 	
 	filterSelection("all")
 	function filterSelection(c) {
-	  var x, i,tr;
-	  x = document.getElementById('pokedex');
-	  if (c == "all") c = "";
-	  tr = x.getElementsByTagName("li");  
-	  for (i = 0; i < tr.length; i++) {
-	 
-		  
-	    w3RemoveClass(tr[i], "show");
-	    if (tr[i].textContent.indexOf(c) > -1) w3AddClass(tr[i], "show");
+	  var x, i,tx;
+	  table = document.getElementById("pokedex");
+	  tx = table.getElementsByTagName("li");
+	  
+	  for(var i=0;i<data.length; i++){
+		tr = data[i].type; 
+		if (c == "all") {
+			tx[i].style.display = "";
+		}
+		else if(tr == c){
+			 tx[i].style.display = "";
+		}
+		 else {
+		        tx[i].style.display = "none";
+		 }
 	  }
-	}
-
-	function w3AddClass(element, name) {
-	  var i, arr1, arr2;
-	  arr1 = element.textContent.split(",");
-	  arr2 = name.split(" ");
-	  for (i = 0; i < arr2.length; i++) {
-	    if (arr1.indexOf(arr2[i]) == -1) {element.textContent += " " + arr2[i];}
-	  }
-	}
-
-	function w3RemoveClass(element, name) {
-	  var i, arr1, arr2;
-	  arr1 = element.textContent.split(",");
-	  arr2 = name.split(" ");
-	  for (i = 0; i < arr2.length; i++) {
-	    while (arr1.indexOf(arr2[i]) > -1) {
-	      arr1.splice(arr1.indexOf(arr2[i]), 1);     
-	    }
-	  }
-	  element.className = arr1.join(" ");
-	}
-
-	// Add active class to the current button (highlight it)
-	var btnContainer = document.getElementById("myBtnContainer");
-	var btns = btnContainer.getElementsByClassName("btn");
-	for (var i = 0; i < btns.length; i++) {
-	  btns[i].addEventListener("click", function(){
-	    var current = document.getElementsByClassName("active");
-	    current[0].className = current[0].className.replace(" active", "");
-	    this.className += " active";
-	  });
-	}
+}
